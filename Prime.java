29ab93948fd030b1744047d920fe1d93edd8ca59
Mayur Desale <158559334+mayurd05@users.noreply.github.com>
2025-07-23 12:37:01 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,24 @@
+class Prime 
+{
+	public static void main(String[] args) 
+	{
+		int a=5;
+		int temp=0;
+		for (int i=2;i<a ;i++ )
+		{
+			if (a%i==0)
+			{
+				temp=temp+1;
+			}
+		}
+		System.out.println(a);
+		if (temp==0)
+		{
+			System.out.println("Number is prime number");
+		}
+		else{
+			System.out.println("it is not prime number");
+		}
+
+	}
+}
